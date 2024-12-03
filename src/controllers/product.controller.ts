fb2225a7cc063a4c1abaad3eab77f0e2276248df
Farhan Rivaldy <fariv.fariv12@gmail.com>
2024-12-04 07:40:38 +0800

    feat: propagation context

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,22 +1,22 @@
 import { Body, Controller, Delete, Get, HttpException, Param, Post, Put, Req, Res, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ProductService } from 'src/services/product.service';
 import { Response } from 'express';
-import opentelemetry from '@opentelemetry/api';
+import { trace } from '@opentelemetry/api';
 
-const tracer = opentelemetry.trace.getTracer('express-app');
 
 
 @Controller('product')
 export class ProductController {
-    constructor (private productService: ProductService){}
+  private readonly tracer = trace.getTracer('product-controller');
+  constructor (private productService: ProductService){}
 
     @Get()
     async findOne(@Res() res: Response) {
-    const span = tracer.startSpan('get-product-route');
+    const span = this.tracer.startSpan('get-product-route');
     span.setAttribute("user_id", "a85faf90-b63b-41ca-ab39-0855969f6ec4")
     span.setAttribute("balance", 800_000_000_000)
     span.end();
-     const data = this.productService.findOne()
+     const data = this.productService.findOne(span)
       return res.status(200).json({
         code: 200,
         status: 'success',
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,34 +1,42 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ProductRepository } from 'src/repositories/product.repo';
-import opentelemetry from '@opentelemetry/api';
+import { Span, context, trace } from '@opentelemetry/api';
 
 @Injectable()
 export class ProductService {
-    private readonly tracer = opentelemetry.trace.getTracer('product-service');
+private readonly tracer = trace.getTracer('product-service');
+  constructor(private readonly productRepo: ProductRepository) {}
 
-    constructor(private readonly productRepo: ProductRepository) { }
+  async findOne(parentSpan: Span) {
 
-    async findOne() {
-        return opentelemetry.context.with(
-            opentelemetry.context.active(),
-            async () => {
-                const span = this.tracer.startSpan('findOne');
-                
-                try {
-                    span.setAttribute('service.method', 'findOne');
-                    
-                    const result = await this.productRepo.findOne();
-                    
-                    span.setAttribute('product.count', result ? 1 : 0);
-                    
-                    return result;
-                } catch (error) {
-                    span.recordException(error);
-                    throw error;
-                } finally {
-                    span.end();
-                }
-            }
-        );
-    }
-} 
+    return context.with(trace.setSpan(context.active(), parentSpan), async () => {
+        const span = this.tracer.startSpan('product-service', {
+          attributes: {
+            'service.method': 'findOne',
+          },
+        });
+
+        span.addEvent("call another service")
+        await this.callAnotherService()
+
+      try {
+        const result = await this.productRepo.findOne();
+
+        span.setAttribute('product.count', result ? 1 : 0);
+
+        return result;
+      } catch (error) {
+        span.recordException(error); 
+        throw error;
+      } finally {
+        span.end();
+      }
+    });
+  }
+
+  async callAnotherService(){
+    setTimeout(() => {
+        // Code to execute after the delay
+        console.log("Service called after 30 seconds");
+      }, 39_000);  }
+}
